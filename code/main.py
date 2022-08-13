5a7e46f0f3573828d336d2739d1427a95546135b
ewait <s2522446388@163.com>
2022-08-13 18:14:34 +0800

    feat(main): add while to def deleteByStartAndEnd,in case more than one (met)

--- code/main.py
+++ code/main.py
@@ -268,14 +268,16 @@ def deleteByStartAndEnd(s, start, end):
     # 找出两个字符串在原始字符串中的位置
     # 开始位置是：开始始字符串的最左边第一个位置；
     # 结束位置是：结束字符串的最右边的第一个位置
-    x1 = s.index(start)
-    x2 = s.index(end,x1+5) + len(end)  # s.index()函数算出来的是字符串的最左边的第一个位置，所以需要加上长度找到末尾
-    # 找出两个字符串之间的内容
-    x3 = s[x1:x2]
-    # 将内容替换为空字符串
-    result = s.replace(x3, "")
-    print(f'Handel{start}: {result}')
-    return result
+    while s.find(start) != -1:
+        x1 = s.find(start)
+        x2 = s.find(end,x1+5) + len(end)  # s.index()函数算出来的是字符串的最左边的第一个位置，所以需要加上长度找到末尾
+        # 找出两个字符串之间的内容
+        x3 = s[x1:x2]
+        # 将内容替换为空字符串s
+        s = s.replace(x3, "")
+
+    print(f'Handel{start}: {s}')
+    return s
 
 
 # 调用翻译,有道和彩云两种引擎（有道寄了就用彩云）
