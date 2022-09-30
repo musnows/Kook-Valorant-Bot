6555dbd0fb1e00251f2d87bcac5d025f8379b7e3
Ewait <s2522446388@163.com>
2022-09-30 09:05:14 +0800

    fix(main): fix log inform err in notify task

--- code/main.py
+++ code/main.py
@@ -2770,9 +2770,11 @@ async def check_notify_err_user(msg:Message):
 async def auto_skin_notify():
     global SkinNotifyDict
     try:
-        print("[BOT.TASK] auto_skin_notify Starting!")  #开始的时候打印一下
+        print("[BOT.TASK.NOTIFY] auto_skin_notify Starting!")  #开始的时候打印一下
         #加载vip用户列表
         VipUserD = copy.deepcopy(VipUserDict)
+        log_vip_failed  = f"[BOT.TASK.NOTIFY] reauthorize failed  = VAu: "
+        log_vip_not_login = f"[BOT.TASK.NOTIFY] not_in UserAuthDict = VAu: "
         #先遍历vip用户列表，获取vip用户的商店
         for vip, uinfo in VipUserD.items():
             try:
@@ -2845,19 +2847,25 @@ async def auto_skin_notify():
                         c.append(Module.Container(Element.Image(src=dailyshop_img_src)))
                         cm.append(c)
                         await user.send(cm)
-                        print(f"[{GetTime()}] Au:{vip} notify_daily_shop successful [{using_time}]")
+                        print(f"[BOT.TASK.NOTIFY] [{GetTime()}] VAu:{vip} notify_shop success [{using_time}]")
                     else:  #reauthorize failed!
-                        print(f"[BOT.TASK] Vip_Au:{vip} user reauthorize failed")
+                        log_vip_failed+=f"({vip})"
+                        #print(f"[BOT.TASK.NOTIFY] Vip_Au:{vip} user reauthorize failed")
                         await user.send(f"尊贵的vip用户，您已登录，但是登录信息失效了。请您重新`login`以查询每日商店\n注：这是无可避免的小概率事件")
                 else:  #不在auth里面说明没有登录
-                    print(f"[BOT.TASK] Vip_Au:{vip} user_not_in UserAuthDict")
+                    log_vip_not_login+=f"({vip})"
+                    #print(f"[BOT.TASK] Vip_Au:{vip} user_not_in UserAuthDict")
                     await user.send(f"尊贵的vip用户，请您`login`来让每日商店提醒生效哦~")
             except Exception as result:  #这个是用来获取单个用户的问题的
-                err_str = f"ERR![BOT.TASK] auto_skin_notify Au:{vip} vip_user.send\n```\n{traceback.format_exc()}\n```"
+                err_str = f"ERR![BOT.TASK.NOTIFY] VAu:{vip} vip_user.send\n```\n{traceback.format_exc()}\n```"
                 print(err_str)
                 await bot.client.send(debug_ch, err_str)  #发送消息到debug频道
-
+        #打印vip的log信息
+        print(log_vip_failed)
+        print(log_vip_not_login)
         # 再遍历所有用户的皮肤提醒
+        log_failed  =   f"[BOT.TASK.NOTIFY] reauthorize failed  = Au: "
+        log_not_login = f"[BOT.TASK.NOTIFY] not_in UserAuthDict = Au: "
         temp_SkinNotifyDict = copy.deepcopy(SkinNotifyDict)
         for aid, skin in temp_SkinNotifyDict['data'].items():
             try:
@@ -2882,21 +2890,23 @@ async def auto_skin_notify():
                         target_skin = [val for key, val in skin.items() if key in list_shop]
                         # print(target_skin)
                         for name in target_skin:
-                            print(f"[BOT.TASK] Au:{aid} auto_skin_notify = {name}")
+                            print(f"[BOT.TASK.NOTIFY] Au:{aid} auto_skin_notify = {name}")
                             await user.send(f"[{GetTime()}] 您的每日商店刷出`{name}`了，请上号查看哦！")
                         # 打印这个说明这个用户正常遍历完了
-                        print(f"[BOT.TASK] Au:{aid} auto_skin_notify = None")
+                        print(f"[BOT.TASK.NOTIFY] Au:{aid} auto_skin_notify = None")
                     else:  #reauthorize failed!
-                        print(f"[BOT.TASK] Vip_Au:{vip} user reauthorize failed")
+                        log_failed+=f"({aid})"
+                        #print(f"[BOT.TASK.NOTIFY] Au:{aid} user reauthorize failed")
                         await user.send(f"您已登录，但是登录信息失效了。请您重新`login`以查询每日商店\n注：这是无可避免的小概率事件")
                 else:  #不在auth里面说明没有登录
-                    print(f"[BOT.TASK] Au:{aid} user_not_in UserAuthDict")
+                    log_not_login+=f"({aid})"
+                    #print(f"[BOT.TASK.NOTIFY] Au:{aid} user_not_in UserAuthDict")
                     await user.send(
                         f"您设置了皮肤提醒，却没有登录！请尽快`login`哦~\n悄悄话: 阿狸会保存vip用户的登录信息，有兴趣[支持一下](https://afdian.net/a/128ahri?tab=shop)吗？"
                     )
             except Exception as result:  #这个是用来获取单个用户的问题的
                 err_cur = str(traceback.format_exc())
-                err_str = f"ERR![BOT.TASK] auto_skin_notify Au:{vip} user.send\n```\n{err_cur}\n```"
+                err_str = f"ERR![BOT.TASK.NOTIFY] Au:{aid} user.send\n```\n{err_cur}\n```"
                 if '屏蔽' in err_cur or '无法发起' in err_cur:
                     del SkinNotifyDict['data'][aid] #直接粗暴解决，删除用户
                     SkinNotifyDict['err_user'][aid] = GetTime()
@@ -2904,13 +2914,16 @@ async def auto_skin_notify():
                 print(err_str)
                 await bot.client.send(debug_ch, err_str)  # 发送消息到debug频道
 
+        #打印普通用户的log信息
+        print(log_failed)
+        print(log_not_login)
         #完成遍历后打印
         if temp_SkinNotifyDict != SkinNotifyDict:
             with open("./log/UserSkinNotify.json", 'w', encoding='utf-8') as fw1:
                 json.dump(SkinNotifyDict, fw1, indent=2, sort_keys=True, ensure_ascii=False)
-            print("[BOT.TASK] save SkinNotifyDict")
+            print("[BOT.TASK.NOTIFY] save SkinNotifyDict")
             
-        finish_str = "[BOT.TASK] auto_skin_notify Finished!"
+        finish_str = "[BOT.TASK.NOTIFY] auto_skin_notify Finished!"
         print(finish_str)  #正常完成
         await bot.client.send(debug_ch, finish_str)  #发送消息到debug频道
     except Exception as result:
