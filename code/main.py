dcb19a85d8ea053683b6f98f7f29fb113c75148b
Ewait <s2522446388@163.com>
2023-01-11 00:18:57 +0800

    refactor(ShopRate): add ShopRate for handling shop_rate

--- code/main.py
+++ code/main.py
@@ -28,7 +28,7 @@ from endpoints.BotVip import (VipUserDict, create_vip_uuid, fetch_vip_user,
                        roll_vip_start, using_vip_uuid, vip_ck, vip_time_remain,
                        vip_time_remain_cm, vip_time_stamp)
 from endpoints.Translate import ListTL,translate_main,Shutdown_TL,checkTL,Open_TL,Close_TL
-
+from endpoints.ShopRate import get_shop_rate_cm,check_shop_rate
 
 # bot的token文件
 with open('./config/config.json', 'r', encoding='utf-8') as f:
@@ -1964,62 +1964,7 @@ async def get_daily_shop(msg: Message, *arg):
             
             #皮肤评分和评价，用户不在err_user里面才显示
             if not check_rate_err_user(msg.author_id):
-                global SkinRateDict
-                rate_text = []
-                rate_count = 0
-                rate_total = 0
-                for sk in list_shop:
-                    if sk in SkinRateDict['rate']:
-                        rate_count+=1
-                        rate_total+=SkinRateDict['rate'][sk]['pit']
-                        skin_name = f"「{SkinRateDict['rate'][sk]['name']}」"
-                        text=f"%-50s\t\t评分: {SkinRateDict['rate'][sk]['pit']}\n"%skin_name
-                        if len(SkinRateDict['rate'][sk]['cmt']) == 1:
-                            ran = 0 #元素内只有1个评论，直接选定该评论
-                        else:
-                            ran = random.randint(0,len(SkinRateDict['rate'][sk]['cmt'])-1)
-                        text+=f"「随机评论」 {SkinRateDict['rate'][sk]['cmt'][ran]}\n"
-                        rate_text.append(text)
-                
-                cm = CardMessage()
-                cm.append(c)
-                if rate_count==0:
-                    rate_lv="皮肤评价数据仍待收集…"
-                    c1 = Card(Module.Header(f"{rate_lv}"),
-                            Module.Context(f"你可以使用「/rate 皮肤名」参与评分哦！"),color='#fb4b57')
-                else:
-                    rate_sum = rate_total//rate_count
-                    #记录当日冠军和屌丝
-                    if rate_sum > SkinRateDict["cmp"]["best"]["pit"]:
-                        SkinRateDict["cmp"]["best"]["pit"] = rate_sum
-                        SkinRateDict["cmp"]["best"]["skin"] = list_shop
-                        SkinRateDict["cmp"]["best"]["kook_id"] = msg.author_id
-                        print(f"[shop] update rate-best  Au:{msg.author_id} = {rate_sum}")
-                    elif rate_sum < SkinRateDict["cmp"]["worse"]["pit"]:
-                        SkinRateDict["cmp"]["worse"]["pit"] = rate_sum
-                        SkinRateDict["cmp"]["worse"]["skin"] = list_shop
-                        SkinRateDict["cmp"]["worse"]["kook_id"] = msg.author_id
-                        print(f"[shop] update rate-worse Au:{msg.author_id} = {rate_sum}")
-                        
-                    if rate_sum>=0 and rate_sum <=20:
-                        rate_lv = "丐帮帮主"
-                    elif rate_sum>20 and rate_sum <=40:
-                        rate_lv = "省钱能手"
-                    elif rate_sum>40 and rate_sum <=60:
-                        rate_lv = "差强人意"
-                    elif rate_sum>60 and rate_sum <=80:
-                        rate_lv = "芜湖起飞"
-                    elif rate_sum>80 and rate_sum <=100:
-                        rate_lv = "天选之人"
-                    c1 = Card(Module.Header(f"综合评分 {rate_sum}，{rate_lv}"),
-                            Module.Context(f"以下评论来自其他用户，仅供图一乐"),
-                            Module.Divider(),color='#fb4b57')
-                    for text in rate_text:
-                        c1.append(Module.Section(Element.Text(text,Types.Text.KMD)))
-                        c1.append(Module.Divider())
-                    c1.append(Module.Context(Element.Text(f"可以使用「/rate 皮肤名」参与评分\n或用「/kkn」查看昨日天选之子/丐帮帮主",Types.Text.KMD)))
-                    
-                cm.append(c1)
+                cm = await get_shop_rate_cm(list_shop,msg.author_id,cm=cm)
                 end = time.perf_counter()#计算获取评分的时间
             # 更新消息
             await upd_card(send_msg['msg_id'], cm, channel_type=msg.channel_type)
@@ -2636,30 +2581,6 @@ async def check_notify_err_user(msg:Message):
     else:
         return False
     
-# 计算用户的得分，插入到当日最高和最低分中
-async def check_shop_rate(user_id:str,list_shop:list):
-    global SkinRateDict
-    rate_count = 0
-    rate_total =0
-    for sk in list_shop:
-        if sk in SkinRateDict['rate']:
-            rate_count+=1
-            rate_total+=SkinRateDict['rate'][sk]['pit']
-    
-    if rate_count !=0:
-        rate_sum = rate_total//rate_count#平均分
-        #记录冠军和屌丝
-        if rate_sum > SkinRateDict["cmp"]["best"]["pit"]:
-            SkinRateDict["cmp"]["best"]["pit"] = rate_sum
-            SkinRateDict["cmp"]["best"]["skin"] = list_shop
-            SkinRateDict["cmp"]["best"]["kook_id"] = user_id
-        elif rate_sum < SkinRateDict["cmp"]["worse"]["pit"]:
-            SkinRateDict["cmp"]["worse"]["pit"] = rate_sum
-            SkinRateDict["cmp"]["worse"]["skin"] = list_shop
-            SkinRateDict["cmp"]["worse"]["kook_id"] = user_id
-        return True
-    else:
-        return False
 
 #独立函数，为了封装成命令+定时
 async def auto_skin_notify():
