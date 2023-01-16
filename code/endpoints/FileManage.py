0e90939ebfb2e73c3b272de5ac68ab7a64fc1632
Ewait <s2522446388@163.com>
2023-01-16 18:12:04 +0800

    feat(EzAuth): add ReadOnly params for FileManage,add append/eq/ne

--- code/endpoints/FileManage.py
+++ code/endpoints/FileManage.py
@@ -19,30 +19,33 @@ def write_file(path:str, value):
         json.dump(value, fw2, indent=2, sort_keys=True, ensure_ascii=False)
 
 # 保存所有文件
-async def Save_All_File():
-    log_text ='[Save.All.File] '
+async def Save_All_File(is_Aio=True):
+    print('[Save.All.File.Aio] start!')
     for i in FileList:
         try:
-            await i.save_aio()
-            log_text+=f"({i.path}) "
+            if is_Aio:
+                await i.save_aio()
+            else:
+                i.save()
         except:
             print(f"ERR! [{GetTime()}] [Save.All.File] {i.path}\n{traceback.format_exc()}")
 
-    log_text+=f"save success at [{GetTime()}]"
-    print(log_text)
+    print(f"[Save.All.File.Aio] save success at [{GetTime()}]")
 
 # 文件管理类
 class FileManage:
     # 初始化构造
-    def __init__(self,path:str) -> None:
+    def __init__(self,path:str,read_only:bool=False) -> None:
         with open(path, 'r', encoding='utf-8') as f:
             tmp = json.load(f)
         self.value = tmp       # 值
         self.type = type(tmp)  # 值的类型
         self.path = path       # 值的文件路径
+        self.Ronly = read_only # 是否只读
         #将自己存全局变量里面
-        global FileList
-        FileList.append(self)
+        if not read_only:
+            global FileList # 如果不是只读，那就存list里面
+            FileList.append(self)
     
     # []操作符重载
     def __getitem__(self,index):
@@ -66,10 +69,16 @@ class FileManage:
         return self.value.__next__()
     # 比较==
     def __eq__(self,i):
-        return self.value.__eq__(i)
+        if isinstance(i,FileManage):
+            return self.value.__eq__(i.value)
+        else:
+            return self.value.__eq__(i)
     # 比较!=
     def __ne__(self,i):
-        return self.value.__ne__(i)
+        if isinstance(i,FileManage):
+            return self.value.__ne__(i.value)
+        else:
+            return self.value.__ne__(i)
 
     # 获取成员
     def get_instance(self):
@@ -78,11 +87,13 @@ class FileManage:
     def items(self):
         return self.value.items()
     # 追加
-    def append(self):
-        self.value.append()
+    def append(self,i):
+        self.value.append(i)
     # list的删除
     def remove(self,i):
         self.value.remove(i)
+    def keys(self):
+        return self.value.keys()
 
     # 保存
     def save(self):
@@ -90,30 +101,30 @@ class FileManage:
             json.dump(self.value, fw, indent=2, sort_keys=True, ensure_ascii=False)
     # 异步保存
     async def save_aio(self):
-        async with aiofiles.open(self.path, 'w', encoding='utf-8') as f:
-            await f.write(json.dump(self.value,indent=2, sort_keys=True, ensure_ascii=False))
+        async with aiofiles.open(self.path, 'w', encoding='utf-8') as f: #这里必须用dumps
+            await f.write(json.dumps(self.value,indent=2, sort_keys=True, ensure_ascii=False))
 
 ###################################################################################################
 
-config = FileManage("./config/config.json")            # 机器人配置文件
-BotUserDict = FileManage("./log/BotUserLog.json")      # 机器人用户信息
-ApiTokenDict = FileManage("./log/ApiToken.json")       # API token
-ColorIdDict = FileManage("./log/color_idsave.json")    # 自动上色 信息保存
-EmojiDict = FileManage("./config/color_emoji.json")    # 自动上色 服务器角色配置
-SponsorDict = FileManage("./log/sponsor_roles.json")   # 感谢助力者 信息保存
+config = FileManage("./config/config.json",True)        # 机器人配置文件
+BotUserDict = FileManage("./log/BotUserLog.json")       # 机器人用户信息
+ApiTokenDict = FileManage("./log/ApiToken.json")        # API token
+ColorIdDict = FileManage("./log/color_idsave.json")     # 自动上色 信息保存
+EmojiDict = FileManage("./config/color_emoji.json",True)# 自动上色 服务器角色配置
+SponsorDict = FileManage("./log/sponsor_roles.json")    # 感谢助力者 信息保存
 
-ValErrDict = FileManage("./log/ValErrCode.json")       # valorant错误码解决办法
-ValSkinList = FileManage("./log/ValSkin.json")         # valorant皮肤
-ValPriceList = FileManage("./log/ValPrice.json")       # valorant皮肤价格
-ValBundleList = FileManage("./log/ValBundle.json")     # valorant捆绑包
-ValItersList = FileManage("./log/ValIters.json")       # valorant皮肤等级
+ValErrDict = FileManage("./log/ValErrCode.json")        # valorant错误码解决办法
+ValSkinList = FileManage("./log/ValSkin.json")          # valorant皮肤
+ValPriceList = FileManage("./log/ValPrice.json")        # valorant皮肤价格
+ValBundleList = FileManage("./log/ValBundle.json")      # valorant捆绑包
+ValItersList = FileManage("./log/ValIters.json")        # valorant皮肤等级
 
 SkinRateDict = FileManage("./log/ValSkinRate.json")     # valorant皮肤评分信息
 SkinNotifyDict = FileManage("./log/UserSkinNotify.json")# 皮肤提醒 用户记录
 GameIdDict = FileManage("./log/game_idsave.json")       # 玩家游戏id保存
 UserTokenDict = FileManage("./log/UserAuthID.json")     # riot用户游戏id和uuid
 
-VipUuidDict = FileManage("./log/VipUuid.json")         # vip uuid文件
-VipUserDict = FileManage("./log/VipUser.json")         # vip 用户列表
-VipShopBgDict = FileManage("./log/VipUserShopBg.json") # vip 背景图设置；商店图缓存
-VipRollDcit = FileManage("./log/VipRoll.json")         # vip 抽奖信息
+VipUuidDict = FileManage("./log/VipUuid.json")          # vip uuid文件
+VipUserDict = FileManage("./log/VipUser.json")          # vip 用户列表
+VipShopBgDict = FileManage("./log/VipUserShopBg.json")  # vip 背景图设置；商店图缓存
+VipRollDcit = FileManage("./log/VipRoll.json")          # vip 抽奖信息
