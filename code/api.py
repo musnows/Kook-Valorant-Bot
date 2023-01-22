07b47d141a3f6677fd708fcef0a159c098f2921f
Ewait <s2522446388@163.com>
2023-01-22 20:34:31 +0800

    feat(api): add indent,sort_keys in json.dumps

--- code/api.py
+++ code/api.py
@@ -12,19 +12,22 @@ routes = web.RouteTableDef()
 @routes.get('/')
 def hello_world(request):  # put application's code here
     print(f"[{GetTime()}] [request] /")
-    return web.Response(body=json.dumps({'code':0,'message': 'Hello! Use path /shop-url or /shop-img to get valorant daily shop','info':'在path后添加/shop-img或者/shop-url来获取每日商店，前者会直接跳转，后者返回一个带图片url的json。示例: /shop-url?account=Riot账户&passwd=Riot密码&img_src=可选参数，自定义背景图'},ensure_ascii=False), status=200, content_type='application/json')
+    return web.Response(body=json.dumps({
+        'code':0,
+        'message': 'Hello! Use path /shop-url or /shop-img to get valorant daily shop','info':'在path后添加/shop-img或者/shop-url来获取每日商店，前者会直接跳转，后者返回一个带图片url的json。示例: /shop-url?account=Riot账户&passwd=Riot密码&img_src=可选参数，自定义背景图','docs':'https://github.com/Aewait/Kook-Valorant-Bot/blob/main/docs/valorant-shop-img-api.md'
+    },indent=2, sort_keys=True,ensure_ascii=False), status=200, content_type='application/json')
 
 # 提供4个皮肤uuid，返回图片
 @routes.get('/shop-draw')
 async def get_dailshop_img(request):
-    print(f"[{GetTime()}] [request] /shop-url")
+    print(f"[{GetTime()}] [request] /shop-draw")
     try:
         ret = await img_draw_request(request)
-        return web.Response(body=json.dumps(ret,ensure_ascii=False), content_type='application/json')
+        return web.Response(body=json.dumps(ret,indent=2, sort_keys=True,ensure_ascii=False), content_type='application/json')
     except:
         err_cur = traceback.format_exc()
         print(f"[{GetTime()}] [Api] ERR in /shop-url\n{err_cur}")
-        return web.Response(body=json.dumps({'code':200,'message': 'unkown err','info':f'未知错误','except':f'{err_cur}'},ensure_ascii=False), content_type='application/json')
+        return web.Response(body=json.dumps({'code':200,'message': 'unkown err','info':f'未知错误','except':f'{err_cur}'},indent=2, sort_keys=True,ensure_ascii=False), content_type='application/json')
 
 
 # 直接跳转图片
@@ -36,11 +39,11 @@ async def get_dailshop_img(request):
         if ret['code']==0:
             return web.Response(headers={'Location': ret['message']},status=303) # 303是直接跳转到图片
         else:
-            return web.Response(body=json.dumps(ret,ensure_ascii=False), content_type='application/json')
+            return web.Response(body=json.dumps(ret,indent=2, sort_keys=True,ensure_ascii=False), content_type='application/json')
     except:
         err_cur = traceback.format_exc()
         print(f"[{GetTime()}] [Api] ERR in /shop-img\n{err_cur}")
-        return web.Response(body=json.dumps({'code':200,'message': 'unkown err','info':f'未知错误','except':f'{err_cur}'},ensure_ascii=False), content_type='application/json')
+        return web.Response(body=json.dumps({'code':200,'message': 'unkown err','info':f'未知错误','except':f'{err_cur}'},indent=2, sort_keys=True,ensure_ascii=False), content_type='application/json')
     
 # 获取图片url
 @routes.get('/shop-url')
@@ -48,22 +51,22 @@ async def get_dailshop_img(request):
     print(f"[{GetTime()}] [request] /shop-url")
     try:
         ret = await login_img_request(request)
-        return web.Response(body=json.dumps(ret,ensure_ascii=False), content_type='application/json')
+        return web.Response(body=json.dumps(ret,indent=2, sort_keys=True,ensure_ascii=False), content_type='application/json')
     except:
         err_cur = traceback.format_exc()
         print(f"[{GetTime()}] [Api] ERR in /shop-url\n{err_cur}")
-        return web.Response(body=json.dumps({'code':200,'message': 'unkown err','info':f'未知错误','except':f'{err_cur}'},ensure_ascii=False), content_type='application/json')
+        return web.Response(body=json.dumps({'code':200,'message': 'unkown err','info':f'未知错误','except':f'{err_cur}'},indent=2, sort_keys=True,ensure_ascii=False), content_type='application/json')
 
 @routes.post('/tfa')
 async def post_tfa_code(request):
     print(f"[{GetTime()}] [request] /tfa")
     try:
         ret = await tfa_code_requeset(request)
-        return web.Response(body=json.dumps(ret,ensure_ascii=False), content_type='application/json')
+        return web.Response(body=json.dumps(ret,indent=2, sort_keys=True,ensure_ascii=False), content_type='application/json')
     except:
         err_cur = traceback.format_exc()
         print(f"[{GetTime()}] [Api] ERR in /tfa\n{err_cur}")
-        return web.Response(body=json.dumps({'code':200,'message': 'unkown err','info':f'未知错误','except':f'{err_cur}'},ensure_ascii=False), content_type='application/json')
+        return web.Response(body=json.dumps({'code':200,'message': 'unkown err','info':f'未知错误','except':f'{err_cur}'},indent=2, sort_keys=True,ensure_ascii=False), content_type='application/json')
     
 from main import bot
 # 爱发电的wh
@@ -72,11 +75,11 @@ async def aifadian_webhook(request):
     print(f"[{GetTime()}] [request] /afd")
     try:
         ret = await afd_request(request,bot)
-        return web.Response(body=json.dumps(ret,ensure_ascii=False), content_type='application/json')
+        return web.Response(body=json.dumps(ret,indent=2, sort_keys=True,ensure_ascii=False), content_type='application/json')
     except:
         err_cur = traceback.format_exc()
         print(f"[{GetTime()}] [Api] ERR in /afd\n{err_cur}")
-        return web.Response(body=json.dumps({"ec":0,"em":"err ouccer"},ensure_ascii=False), content_type='application/json')
+        return web.Response(body=json.dumps({"ec":0,"em":"err ouccer"},indent=2, sort_keys=True,ensure_ascii=False), content_type='application/json')
 
 
 print(f"[API Start] starting at {GetTime()}")
