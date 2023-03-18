a1480ec526315ef0629ece98a53d9209b6d0a10b
musnow <ezplayingd@126.com>
2023-03-18 10:30:15 +0800

    feat(plugins): add BotStatus to pkg.plugins

--- code/main.py
+++ code/main.py
@@ -142,43 +142,6 @@ async def atAhri(msg: Message):
 ###########################################################################################
 
 
-# 开始打游戏
-@bot.command()
-async def gaming(msg: Message, game: int = 1):
-    BotLog.logMsg(msg)
-    #await bot.client.update_playing_game(3,1)# 英雄联盟
-    if game == 1:
-        ret = await status_active_game(453027)  # 瓦洛兰特
-        await msg.reply(f"{ret['message']}，阿狸上号valorant啦！")
-    elif game == 2:
-        ret = await status_active_game(3)  # 英雄联盟
-        await msg.reply(f"{ret['message']}，阿狸上号LOL啦！")
-
-
-# 开始听歌
-@bot.command()
-async def singing(msg: Message, music: str = "err", singer: str = "err"):
-    BotLog.logMsg(msg)
-    if music == "err" or singer == "err":
-        await msg.reply(f"函数参数错误，music: `{music}` singer: `{singer}`")
-        return
-
-    ret = await status_active_music(music, singer)
-    await msg.reply(f"{ret['message']}，阿狸开始听歌啦！")
-
-
-# 停止打游戏1/听歌2
-@bot.command(name='sleeping')
-async def sleeping(msg: Message, d: int = 1):
-    BotLog.logMsg(msg)
-    ret = await status_delete(d)
-    if d == 1:
-        await msg.reply(f"{ret['message']}，阿狸下号休息啦!")
-    elif d == 2:
-        await msg.reply(f"{ret['message']}，阿狸摘下了耳机~")
-    #await bot.client.stop_playing_game()
-
-
 # 拳头api调用被禁止的时候用这个变量取消所有相关命令
 async def LoginForbidden_send(msg: Message):
     _log.info(f"Au:{msg.author_id} Command Failed")
@@ -2133,7 +2096,7 @@ async def proc_check(msg: Message, *arg):
 
 #在阿狸开机的时候自动加载所有保存过的cookie
 # 注册其他命令
-from pkg.plugins import Funny,GrantRoles,Translate
+from pkg.plugins import Funny,GrantRoles,Translate,BotStatus
 
 @bot.on_startup
 async def loading_cache(bot: Bot):
@@ -2145,6 +2108,7 @@ async def loading_cache(bot: Bot):
         Funny.init(bot,debug_ch)
         GrantRoles.init(bot,master_id)
         Translate.init(bot,master_id)
+        BotStatus.init(bot)
         _log.info("[BOT.TASK] load plugins")
     except:
         _log.fatal("[BOT.TASK] startup task failed!")
---
+++ code/pkg/plugins/BotStatus.py
@@ -0,0 +1,40 @@
+from khl import Bot,Message
+from ..utils.log import BotLog
+from ..utils.KookApi import status_active_game,status_active_music,status_delete
+
+def init(bot:Bot):
+    # 开始打游戏
+    @bot.command(name="gaming")
+    async def gaming(msg: Message, game: int = 1):
+        BotLog.logMsg(msg)
+        #await bot.client.update_playing_game(3,1)# 英雄联盟
+        if game == 1:
+            ret = await status_active_game(453027)  # 瓦洛兰特
+            await msg.reply(f"{ret['message']}，阿狸上号valorant啦！")
+        elif game == 2:
+            ret = await status_active_game(3)  # 英雄联盟
+            await msg.reply(f"{ret['message']}，阿狸上号LOL啦！")
+
+
+    # 开始听歌
+    @bot.command(name="singing")
+    async def singing(msg: Message, music: str = "err", singer: str = "err"):
+        BotLog.logMsg(msg)
+        if music == "err" or singer == "err":
+            await msg.reply(f"函数参数错误，music: `{music}` singer: `{singer}`")
+            return
+
+        ret = await status_active_music(music, singer)
+        await msg.reply(f"{ret['message']}，阿狸开始听歌啦！")
+
+
+    # 停止打游戏1/听歌2
+    @bot.command(name='sleeping')
+    async def sleeping(msg: Message, d: int = 1):
+        BotLog.logMsg(msg)
+        ret = await status_delete(d)
+        if d == 1:
+            await msg.reply(f"{ret['message']}，阿狸下号休息啦!")
+        elif d == 2:
+            await msg.reply(f"{ret['message']}，阿狸摘下了耳机~")
+        #await bot.client.stop_playing_game()
