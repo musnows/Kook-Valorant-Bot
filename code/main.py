ee211910317203a677fc2c7a6ccaeab5c53b7e5e
musnow <ezplayingd@126.com>
2023-03-24 12:36:17 +0800

    feat(mission): add pkg.plugins.mission for getting misson from cur database

--- code/main.py
+++ code/main.py
@@ -862,57 +862,6 @@ async def get_user_card(msg: Message, *arg):
         await BotLog.BaseException_Handler("uinfo", traceback.format_exc(), msg, send_msg=send_msg)
 
 
-@bot.command(name='mission',case_sensitive=False)
-async def mission(msg:Message,*arg):
-    BotLog.logMsg(msg)
-    send_msg = {'msg_id':''}
-    try:
-        # 1.如果用户不在Authdict里面，代表没有登录，直接退出
-        if msg.author_id not in UserAuthCache['kook']:
-            await msg.reply(await get_card_msg("您尚未登陆！请「私聊」使用login命令进行登录操作", f"「/login 账户 密码」请确认您知晓这是一个风险操作", icon_cm.whats_that)) # type:ignore
-            return
-
-        # 2.直接使用for循环来获取不同用户的信息
-        cm = CardMessage()
-        text = ""
-        for riot_user_id in UserAuthCache['kook'][msg.author_id]:
-            try:
-                # 3.执行cookie重登
-                reau = await Reauth.check_reauth("玩家任务",msg.author_id,riot_user_id,debug_ch,msg)
-                if reau == False: return  #如果为假说明重新登录失败
-                # 3.2 获取玩家id成功了，再提示正在获取商店
-                cm = await get_card_msg("正在尝试获取您的每日任务", "阿狸正在施法，很快就好啦！", icon_cm.duck)
-                # 3.2.1 如果reauth函数return的是dict，说明重新登录成功且发送了消息，则更新卡片
-                if isinstance(reau, dict):  
-                    await upd_card(reau['msg_id'], cm, channel_type=msg.channel_type)
-                    send_msg = reau
-                # 3.2.1 不是dict，说明不需要重登，也没有发送提示信息
-                else:
-                    send_msg = await msg.reply(cm)  #记录消息id用于后续更新
-
-                auth = UserAuthCache['data'][riot_user_id]['auth']
-                assert isinstance(auth, EzAuth)
-                # 获取玩家任务
-                ret = await Riot.fetch_player_contract(auth.get_riotuser_token())
-                m = ret["Missions"]
-                id_text = f"{riot_user_id}_{getTime()}"
-                text += f"{auth.Name}#{auth.Tag} = {id_text}\n"
-                write_file(f'./log/mission/{id_text}.json',ret)
-                _log.info(f"Au:{msg.author_id} | get {riot_user_id} mission success")
-            except KeyError as result:
-                if "Missions" in str(result):
-                    _log.exception(f"KeyErr while Au:{msg.author_id} | Ru:{riot_user_id}")
-                    cm2 = await get_card_msg(f"键值错误，需要重新登录", f"KeyError:{result}, please re-login", icon_cm.lagging)
-                    await upd_card(send_msg['msg_id'], cm2, channel_type=msg.channel_type)
-        
-        # 多个账户都获取完毕，发送卡片并输出结果
-        cm = await get_card_msg("任务获取成功，感谢您对开发的贡献！", f"请转到[金山表单](https://f.wps.cn/g/Fipjms3w/)填写相关信息\n填写表单时，提供下方=右侧编号即可\n```\n{text}\n```")
-        await upd_card(send_msg['msg_id'], cm, channel_type=msg.channel_type)
-        _log.info(f"Au:{msg.author_id} | mission reply successful!")
-    except Exception as result:
-        await BotLog.BaseException_Handler("mission",traceback.format_exc(),msg)
-
-
 # 获取捆绑包信息(无需登录)
 @bot.command(name='bundle', aliases=['skin'])
 async def get_bundle(msg: Message, *arg):
@@ -1602,7 +1551,7 @@ async def proc_check(msg: Message, *arg):
 
 #在阿狸开机的时候自动加载所有保存过的cookie
 # 注册其他命令
-from pkg.plugins import Funny,GrantRoles,Translate,BotStatus,Vip,Match,GameHelper,ValFileUpd
+from pkg.plugins import Funny,GrantRoles,Translate,BotStatus,Vip,Match,GameHelper,ValFileUpd,Mission
 
 @bot.on_startup
 async def loading_cache(bot: Bot):
@@ -1619,6 +1568,7 @@ async def loading_cache(bot: Bot):
         GameHelper.init(bot)
         ValFileUpd.init(bot,bot_upimg,master_id)
         Vip.init(bot,bot_upimg,master_id,debug_ch,cm_send_test)
+        Mission.init(bot,debug_ch)
         _log.info("[BOT.TASK] load plugins")
     except:
         _log.fatal("[BOT.TASK] startup task failed!")
