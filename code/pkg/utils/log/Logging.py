72881c2a837f632e549ae070b28fde00721cc23e
musnow <ezplayingd@126.com>
2023-12-30 19:41:13 +0800

    feat(logging): add pid in log, change log rolling interval to 3 days

--- code/pkg/utils/log/Logging.py
+++ code/pkg/utils/log/Logging.py
@@ -1,8 +1,11 @@
 import logging # 采用logging来替换所有print
+from os import getpid
 from logging.handlers import TimedRotatingFileHandler
 
 LOGGER_NAME = "botlog"
 LOGGER_FILE = "bot.log"
+LOGGER_BOT_PID = getpid()
+"""机器人进程pid"""
 
 # 只打印info以上的日志（debug低于info）
 logging.basicConfig(level=logging.INFO,
@@ -14,12 +17,13 @@ _log = logging.getLogger(LOGGER_NAME)
 # 实例化控制台handler和文件handler，同时输出到控制台和文件
 # cmd_handler = logging.StreamHandler() # 默认设置里面，就会往控制台打印信息;自己又加一个，导致打印俩次
 file_handler = logging.FileHandler(LOGGER_FILE, mode="a", encoding="utf-8")
-fmt = logging.Formatter(fmt="[%(asctime)s] %(levelname)s:%(filename)s:%(funcName)s:%(lineno)d | %(message)s",
+fmt = logging.Formatter(fmt="[%(asctime)s] [%(bot_pid)d] %(levelname)s:%(filename)s:%(funcName)s:%(lineno)d | %(message)s",
                     datefmt="%y-%m-%d %H:%M:%S")
 file_handler.setFormatter(fmt)
 # 每天新生成一个
-log_handler = TimedRotatingFileHandler(LOGGER_FILE, when='D')
+log_handler = TimedRotatingFileHandler(LOGGER_FILE, when='D',interval=3)
 log_handler.setFormatter(fmt)
 
 # _log.addHandler(file_handler)
 _log.addHandler(log_handler)
+_log = logging.LoggerAdapter(_log, {"bot_pid": LOGGER_BOT_PID})
